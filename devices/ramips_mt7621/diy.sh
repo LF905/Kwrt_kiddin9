75e27c3fa28ab0658996af904fce6cc3ca87ecc6
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-04-24 03:25:27 +0800

    update

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -10,6 +10,4 @@ sed -i "s/DEVICE_MODEL := HC5962$/DEVICE_MODEL := HC5962 \/ B70/" target/linux/r
 
 sed -i '/# start dockerd/,/# end dockerd/d' .config
 
-sed -i "s/KERNEL_SIZE := 3145728/KERNEL_SIZE := 3407564/" target/linux/ramips/image/mt7621.mk
-
 sed -i "s/--max-leb-cnt=96/--max-leb-cnt=128/g" target/linux/ramips/image/mt7621.mk
